9c4d18db43628a4ae4dd45a79e6d9822fe445ef2
Dom Baker <dominic.baker1@nhs.net>
2022-09-21 21:49:35 +0100

    added more functionality

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
 import pytest
+import os
+import tempfile
+import website
 from website.models import User, Ticket
 from werkzeug.security import generate_password_hash
 
@@ -12,3 +15,16 @@ def new_user():
 def new_ticket():
     ticket = Ticket(title="sometitle", description="somedescription", area_of_business="HR")
     return ticket
+
+@pytest.fixture
+def client():
+    db_fd, website.app.config['DATABASE'] = tempfile.mkstemp()
+    website.app.config['TESTING'] = True
+
+    with website.app.test_client() as client:
+        with website.app.app_context():
+            website.init_db()
+        yield client
+
+    os.close(db_fd)
+    os.unlink(website.app.config['DATABASE'])
